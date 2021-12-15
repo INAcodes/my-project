15dc96f921b063009112def3d244168723b26232
INAcodes <93144864+INAcodes@users.noreply.github.com>
2021-12-15 23:55:41 +0100

    add java script

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 function displayTemperature(response){
-  console.log(response.data.main.temp);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
+
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement = setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function getForecast(coordinates) {
+  let urlBase = "https://api.openweathermap.org/data/2.5/onecall?";
+  let apiKey = "c73239399918e583c389935335c11e48";
+  let lat = coordinates.lat;
+  let lon = coordinates.lon;
+  let apiUrl = `${urlBase}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 
-
-
-
-
-let apiKey = "d17fa40e825be2790f261dab24f52143";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric";
+function search (city) {
+  let apiKey = "c73239399918e583c389935335c11e48";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
