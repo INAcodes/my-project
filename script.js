9bdbaa5ac364f6d8a06b0cb3ac56f38e96f959fa
INAcodes <93144864+INAcodes@users.noreply.github.com>
2021-12-18 11:36:03 +0100

    added function displayForecast

--- script.js
+++ script.js
@@ -14,26 +14,34 @@ function formatDate(timestamp){
 
 
 }
-function displayForecast (){
-  let forecastElement= document.querySelector("#forecast");
-  forecastElement.innerHTML = `
-  <div class="row">
-				<div class="col-2">
-          <div class="weather-forcast-date">
-            Thu
-          </div>
-          <img 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
           src="http://openweathermap.org/img/wn/50d@2x.png"
           alt=""
-          width="36"
-          />
-          <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max">18°</span>
-            <span class="weather-forecast-temperature-min">12°</span>
-          </div>
-			</div>
-      </div>		
-      `;
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 function displayTemperature(response){
